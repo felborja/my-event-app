8dbe6a5847e16a597244955ec5dea98390a5c642
AmarbayarBold <abnabayar@gmail.com>
2024-07-04 11:41:44 +0200

    Authenticated user can now sign out

--- my-event-app/src/components/Layout/LayoutProtected.jsx
+++ my-event-app/src/components/Layout/LayoutProtected.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Navigation from "../common/Navigation";
 import MinimalFooter from "../common/MinimalFooter";
 
 const LayoutProtected = ({ children }) => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col min-h-screen">
-      <Navigation isLoggedIn={true} />
+      <Navigation
+        isLoggedIn={true}
+        onSignOut={() => {
+          if (window.confirm("Are you sure you want to sign out?")) {
+            localStorage.removeItem("authToken");
+            navigate("/");
+          }
+        }}
+      />
       <main className="container mx-auto p-4 flex-grow">{children}</main>
       <MinimalFooter />
     </div>
